eca5baf20d34bbdb4b29ed5ef6d8041bc940e7c2
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2025-11-07 14:15:55 +0100

    ok

--- tworzenie stron i aplikacji internetowych/pole_trojkata/pole_trojkata.php
+++ tworzenie stron i aplikacji internetowych/pole_trojkata/pole_trojkata.php
@@ -7,14 +7,14 @@
 </head>
 <body>
 <?php
-// Define the base and height
+
 $a = 12345679;
 $h = 144;
 
-// Calculate the area of the triangle
+
 $pole = (1/2) * $a * $h;
 
-// Display the result
+
 echo "<address>Pole trójkąta o podstawie $a i wysokości równej $h wynosi $pole j2</address>";
 ?>
 </body>

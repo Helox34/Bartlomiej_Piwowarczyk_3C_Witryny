d76d24695c94f128c8871505fe290b8a5d8cb56c
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2025-11-07 13:37:49 +0100

    ds

--- tworzenie stron i aplikacji internetowych/prostopadłościan/prostopadloscian.php
+++ tworzenie stron i aplikacji internetowych/prostopadłościan/prostopadloscian.php
@@ -7,17 +7,12 @@
 </head>
 <body>
 <?php
-
 $a = 5;
 $b = 12;
 $c = 84;
-
 $diagonal = sqrt(pow($a, 2) + pow($b, 2) + pow($c, 2));
-
 echo "<p>Przekątna prostopadłościanu o krawędziach długości $a, $b i $c jest równa $diagonal.</p>";
-
 $surfaceArea = 2 * ($a * $b + $b * $c + $a * $c);
-
 echo "<p>Pole powierzchni prostopadłościanu o krawędziach długości $a, $b i $c wynosi $surfaceArea j<sup>2</sup>.</p>";
 ?>
 

ee4672fd2ae3bc90051330ac80c4e288c0b0a13d
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2025-11-07 12:38:21 +0100

    pole trojkata

---
+++ tworzenie stron i aplikacji internetowych/pole_trojkata/pole_trojkata.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+$poczatek = 'Ala ma ';
+$liczba = 4;
+$koniec = ' koty';
+
+echo '$poczatek $liczba $koniec a chce mieć jeszcze dwa.';
+
+echo "$poczatek $liczba $koniec a chce mieć jeszcze dwa.";
+?>
+
+</body>
+</html>

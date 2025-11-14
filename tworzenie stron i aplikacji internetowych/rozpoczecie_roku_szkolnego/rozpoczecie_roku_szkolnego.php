b7764382f196aa2e899b077d9706fe6826f28739
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2025-11-14 14:30:39 +0100

    aa

--- tworzenie stron i aplikacji internetowych/rozpoczecie_roku_szkolnego/rozpoczecie_roku_szkolnego.php
+++ tworzenie stron i aplikacji internetowych/rozpoczecie_roku_szkolnego/rozpoczecie_roku_szkolnego.php
@@ -1,12 +1,4 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Document</title>
-</head>
-<body>
-    <?php
+<?php
 $today = time();
 $month = date('n', $today);
 $year = date('Y', $today);
@@ -20,9 +12,14 @@ if ($month >= 9) {
 $school_start = mktime(0, 0, 0, 9, 1, $school_year);
 $day_name = date('l', $school_start);
 $day_of_year = date('z', $school_start) + 1;
-    ?>
-<p style="background-color: lightsteelblue; border: 2px dotted blueviolet; border-radius: 5px; font-style: italic; padding: 15px;">
-    Rozpoczęcie tego roku szkolnego odbyło się w <?php  $day_name; ?> i był to <?php=$day_of_year; ?> dzień roku.</p>
-
+?>
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="UTF-8">
+<title>Rozpoczęcie roku szkolnego</title>
+</head>
+<body>
+<p style="background-color: lightsteelblue; border: 2px dotted blueviolet; border-radius: 5px; font-style: italic; padding: 15px;">Rozpoczęcie tego roku szkolnego odbyło się w <?php echo $day_name; ?> i był to <?php echo $day_of_year; ?> dzień roku.</p>
 </body>
 </html>

08f95477cc6edd81b748dae0b6fda2ce428e50e4
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2026-05-22 14:38:59 +0200

    feat: remove outdated PHP files and add new implementation for employee hiring data

--- tworzenie stron i aplikacji internetowych/zadanie 15.05.2026/zadanie_10.php
+++ tworzenie stron i aplikacji internetowych/zadanie 15.05.2026/zadanie_10.php
@@ -1,50 +1,51 @@
-<!DOCTYPE html>
+
+<!doctype html>
 <html lang="pl">
 <head>
-    <meta charset="UTF-8">
-    <title>Zadanie 6 - Nazwy stanowisk</title>
-    <style>
-        table, th, td {
-            border: 1px solid black;
-            border-collapse: collapse;
-        }
-        th, td {
-            padding: 5px;
-        }
-        .right-align {
-            text-align: right;
-        }
-    </style>
+  <meta charset="utf-8">
+  <title>Data zatrudnienia</title>
 </head>
 <body>
-    <h2>Zestawienie stanowisk i liczby liter</h2>
-    <?php
-    $conn = mysqli_connect('localhost', 'piwowarczyk', 'haslo123', 'piwowarczyk');
-    
-    if (!$conn) {
-        die("Błąd połączenia: " . mysqli_connect_error());
-    }
-    
-    mysqli_set_charset($conn, "utf8mb4");
 
-    $query = "SELECT Nazwa, CHAR_LENGTH(Nazwa) AS Liczba_liter FROM Stanowiska";
-    $result = mysqli_query($conn, $query);
+<?php
+$servername = 'localhost';
+$username = 'piwowarczyk';
+$password = '1234';
+$database = 'piwowarczyk';
 
-    if (mysqli_num_rows($result) > 0) {
-        echo '<table>';
-        echo '<tr><th>Nazwa stanowiska</th><th>Liczba liter</th></tr>';
-        while ($row = mysqli_fetch_assoc($result)) {
-            echo '<tr>';
-            echo '<td>' . htmlspecialchars($row['Nazwa']) . '</td>';
-            echo '<td class="right-align">' . $row['Liczba_liter'] . ' liter</td>';
-            echo '</tr>';
-        }
-        echo '</table>';
-    } else {
-        echo 'Brak danych';
-    }
+$conn = mysqli_connect($servername, $username, $password, $database);
 
-    mysqli_close($conn);
-    ?>
+if (!$conn) {
+	die('Błąd połączenia: ' . mysqli_connect_error());
+}
+
+mysqli_set_charset($conn, 'utf8');
+
+$sql = "SELECT Imie, Nazwisko, Data_zatrudnienia FROM pracownicy ORDER BY Data_zatrudnienia";
+$result = mysqli_query($conn, $sql);
+
+echo "<h2>Data zatrudnienia</h2>";
+echo "<table border='1' cellpadding='5' cellspacing='0'>";
+echo "<tr><th>Imię</th><th>Nazwisko</th><th>Data zatrudnienia</th></tr>";
+
+if ($result && mysqli_num_rows($result) > 0) {
+	while ($row = mysqli_fetch_assoc($result)) {
+		echo "<tr>";
+		echo "<td>" . htmlspecialchars($row['Imie']) . "</td>";
+		echo "<td>" . htmlspecialchars($row['Nazwisko']) . "</td>";
+		echo "<td>" . htmlspecialchars($row['Data_zatrudnienia']) . "</td>";
+		echo "</tr>";
+	}
+} else {
+	echo "<tr><td colspan='3'>Brak wyników</td></tr>";
+}
+
+echo "</table>";
+
+mysqli_close($conn);
+
+?>
+
 </body>
 </html>
+

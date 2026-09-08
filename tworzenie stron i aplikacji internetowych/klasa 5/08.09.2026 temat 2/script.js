9e3d5272a1064a760ef5a32d9d279d810c19f215
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2026-09-08 11:12:07 +0200

    pierwsze lekcjie js

---
+++ tworzenie stron i aplikacji internetowych/klasa 5/08.09.2026 temat 2/script.js
@@ -0,0 +1,12 @@
+let  alphabet = ["a", "b", "c", "d", "e",];
+
+// console.log(alphabet.length)
+// console.log(alphabet[0])
+// console.log(alphabet[1])
+// console.log(alphabet[2])
+// console.log(alphabet[3])
+// console.log(alphabet[4])
+
+for(let  i=0; i<alphabet.length; i++){
+    console.log(alphabet[i])
+}

13a87df9d7e1b29281d5e28060a6ae3792ae296e
Helox34 <bartlomiejpiwowarczyk5@gmail.com>
2026-09-08 11:13:52 +0200

    1 lekcja

---
+++ tworzenie stron i aplikacji internetowych/klasa 5/08.09.2026 temat 1/script.js
@@ -0,0 +1,30 @@
+// 1.
+// let imie = prompt("podaj swoje imie");
+// alert("witaj" + imie + "!");
+
+// let nazwisko = prompt("podaj swoje nazwisko ");
+// alert("wiaj + nazwisko + "!");
+
+// let miasto = prompt("podaj swoje imie");
+// alert("jestes" + miasto + "!");
+
+// let ulica = prompt("podaj swoje nazwisko ");
+// alert("jesteś na" + ulica + "!");
+
+// console.log(st);
+
+
+// 2.
+// let a = parseInt(prompt(`podaj liczbe a`))
+// let b = parseInt(prompt(`podaj liczbe b`))
+
+// console.log(a+b)
+// console.log(a-b)
+// console.log(a*b)
+// console.log(a/b)
+// console.log(a%b)
+// console.log(b-a)
+// console.log(b/a)
+// console.log(b%a)
+
+// 3.
---
+++ tworzenie stron i aplikacji internetowych/klasa 5/08.09.2026 temat 1/script_zadanie.js
@@ -0,0 +1,28 @@
+//zadaanie 1
+let wiadomosc = prompt("podaj wiadomość");
+console.log(wiadomosc) 
+
+//zadaanie 2
+let imie = "Bartłomiej"
+console.log(imie) 
+let nazwisko = "Piwowarczyk"
+console.log(nazwisko) 
+let wiek = "19"
+console.log(wiek) 
+let klasa = "4C"
+console.log(klasa) 
+
+//zadanie 3
+let a = parseInt(prompt(`podaj liczbe a`))
+let b = parseInt(prompt(`podaj liczbe b`))
+
+console.log(a+b)
+console.log(a-b)
+console.log(a*b)
+console.log(a/b)
+
+//zadanie 4
+if(wiek >= 18)
+console.log("pełnoletni")
+else
+console.log("niepełnoletni")
